d4ca28d5553d5bd4dc6c6443f387a4463fced88f
Eliot Swank <eliotswank@gmail.com>
2018-08-13 17:22:45 -0600

    adds test setup for existing assertions

--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 describe 'Merchants API' do
   context "GET /api/v1" do
     it 'returns a list of merchants' do
+      create_list(:merchant, 3)
+
       get "/api/v1/merchants"
 
       expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+      merchant = merchants.first
+      
       expect(merchants.count).to eq(3)
       expect(merchant).to have_key(:name)
     end
