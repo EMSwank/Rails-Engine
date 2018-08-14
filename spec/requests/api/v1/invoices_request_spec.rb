90fd996672b08517a5ea465a47d777a38a84812f
Chris Powell <chris.ian.powell@gmail.com>
2018-08-14 14:25:22 -0600

    adds single finders for invoices

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -50,7 +50,7 @@ describe 'Invoices API' do
 
       expect(invoice['customer_id']).to eq(c_id)
     end
-    it 'displays a single json object by customer_id' do
+    it 'displays a single json object by merchant_id' do
       m_id = create(:invoice).merchant_id
 
       get "/api/v1/invoices/find?merchant_id=#{m_id}"
@@ -60,6 +60,39 @@ describe 'Invoices API' do
       invoice = JSON.parse(response.body)
 
       expect(invoice['merchant_id']).to eq(m_id)
+    end
+    it 'displays a single json object by status' do
+      stts = create(:invoice).status
+
+      get "/api/v1/invoices/find?status=#{stts}"
+
+      expect(response).to be_successful
+
+      invoice = JSON.parse(response.body)
+
+      expect(invoice['status']).to eq(stts)
+    end
+    it 'displays a single json object by created_at' do
+      invoice_thing = create(:invoice, created_at: "2012-03-09 08:57:21 UTC")
+
+      get "/api/v1/invoices/find?created_at=#{invoice_thing.created_at}"
+
+      expect(response).to be_successful
+
+      invoice = JSON.parse(response.body)
+
+      expect(invoice['created_at']).to eq("2012-03-09T08:57:21.000Z")
+    end
+    it 'displays a single json object by updated_at' do
+      invoice_thing = create(:invoice, updated_at: "2012-03-09 08:57:21 UTC")
+
+      get "/api/v1/invoices/find?updated_at=#{invoice_thing.updated_at}"
+
+      expect(response).to be_successful
+
+      invoice = JSON.parse(response.body)
+
+      expect(invoice['updated_at']).to eq("2012-03-09T08:57:21.000Z")
     end
   end
 end
