8882bc8e8ed0408cab8658e32800418e925a1871
Eliot Swank <eliotswank@gmail.com>
2018-08-14 14:20:54 -0600

    adds routes and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/api/v1/merchants/find?', to: 'merchants#show'
+      get '/merchants/find', to: 'merchants/search#show'
       resources :merchants, only: [:index, :show]
       get '/api/v1/transactions/find?', to: 'transactions#show'
       resources :transactions, only: [:index, :show]
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
