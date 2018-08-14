c6f1d876c94865ede82b5bfcb2ddc3f91179891d
Eliot Swank <eliotswank@gmail.com>
2018-08-14 13:04:19 -0600

    adds model validations and transaction index feature

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :transaction do
     invoice_id 1
     credit_card_number Faker::Business.credit_card_number
-    credit_card_expiration_date Faker::Business.credit_card_expiry_date
     result "success"
   end
 end
--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -4,21 +4,20 @@ describe "Transactions API" do
   context "GET /api/v1" do
     it "returns a list of transactions" do
       invoice = create(:invoice)
-      create_list(:transaction, 3, invoice_id: invoice.id)
-
-byebug
+      3.times do
+        create(:transaction, invoice_id: invoice.id)
+      end
 
       get "/api/v1/transactions"
 
       expect(response).to be_successful
 
       transactions = JSON.parse(response.body, symbolize_names: true)
-      transaction = transaction.first
+      transaction = transactions.first
 
       expect(transactions.count).to eq(3)
       expect(transaction).to have_key(:invoice_id)
       expect(transaction).to have_key(:credit_card_number)
-      expect(transaction).to have_key(:credit_card_expiration_date)
       expect(transaction).to have_key(:result)
     end
   end
