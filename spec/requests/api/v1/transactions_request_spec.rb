0653ab8bc3ab63d6e7249338e02c4c95cb4d665b
Eliot Swank <eliotswank@gmail.com>
2018-08-14 12:05:35 -0600

    adds request spec

---
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "Transactions API" do
+  context "GET /api/v1" do
+    it "returns a list of merchants" do
+      create_list(:transaction, 3)
+
+      get "/api/v1/transactions"
+
+      expect(response).to be_successful
+
+      transactions = JSON.parse(response.body, symbolize_names: true)
+      transaction = transaction.first
+
+      expect(transactions.count).to eq(3)
+      expect(merchant).to have_key(:invoice_id)
+      expect(merchant).to have_key(:credit_card_number)
+      expect(merchant).to have_key(:credit_card_expiration_date)
+      expect(merchant).to have_key(:result)
+    end
+  end
+end
