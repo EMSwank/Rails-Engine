ae556a100a7db13cdef001022c227f91b79797b8
Eliot Swank <eliotswank@gmail.com>
2018-08-14 14:24:14 -0600

    adds find_by_name functionality to merchant controller

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
     render json: Merchant.find(params[:id]) if params[:id]
+    render json: Merchant.find_by_name(params[:name]) if params[:name]
   end
 end
