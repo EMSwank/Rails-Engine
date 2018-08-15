e71489953f30e0c0cc3968357705180f6b12ce06
Eliot Swank <eliotswank@gmail.com>
2018-08-15 12:04:11 -0600

    implements find_by_updated_at

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -5,6 +5,7 @@ class Api::V1::Customers::SearchController < ApplicationController
     render json: Customer.find_by_first_name(params[:first_name]) if params[:first_name]
     render json: Customer.find_by_last_name(params[:last_name]) if params[:last_name]
     render json: Customer.find_by_created_at(params[:created_at]) if params[:created_at]
+    render json: Customer.find_by_updated_at(params[:updated_at]) if params[:updated_at]
 
   end
 end
