f1c6235dcceae08bfe53591ffaab71053ea44027
Chris Powell <chris.ian.powell@gmail.com>
2018-08-14 18:46:34 -0600

    adds find multi functionality for invoices

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get '/api/v1/transactions/find?', to: 'transactions#show'
       resources :transactions, only: [:index, :show]
       get '/invoices/find', to: 'invoices/search#show'
+      get '/invoices/find_all', to: 'invoices/search#index'
       resources :invoices, only: [:index, :show]
     end
   end
--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -95,4 +95,95 @@ describe 'Invoices API' do
       expect(invoice['updated_at']).to eq("2012-03-09T08:57:21.000Z")
     end
   end
+  context 'GET /api/v1/invoices/find_all?parameters' do
+    it 'displays a all json objects by id' do
+      invoice_1 = create(:invoice, id: 1)
+      invoice_2 = create(:invoice, id: 2)
+      invoice_3 = create(:invoice, id: 3)
+
+      get "/api/v1/invoices/find_all?id=1"
+
+      expect(response).to be_successful
+
+      invoice = JSON.parse(response.body)
+
+      expect(invoice.length).to eq(1)
+    end
+    it 'displays all json objects by customer_id' do
+      customer = create(:customer)
+      create(:invoice, customer_id: customer.id)
+      create(:invoice, customer_id: customer.id)
+      create(:invoice, customer_id: customer.id)
+
+      get "/api/v1/invoices/find_all?customer_id=#{customer.id}"
+
+      expect(response).to be_successful
+
+      invoice = JSON.parse(response.body)
+
+      expect(invoice.length).to eq(3)
+      expect(invoice.first['customer_id']).to eq(customer.id)
+      expect(invoice.last['customer_id']).to eq(customer.id)
+    end
+    it 'displays all json objects by merchant_id' do
+      merchant = create(:merchant)
+      create(:invoice, merchant_id: merchant.id)
+      create(:invoice, merchant_id: merchant.id)
+      create(:invoice, merchant_id: merchant.id)
+
+      get "/api/v1/invoices/find_all?merchant_id=#{merchant.id}"
+
+      expect(response).to be_successful
+
+      invoice = JSON.parse(response.body)
+
+      expect(invoice.length).to eq(3)
+      expect(invoice.first['merchant_id']).to eq(merchant.id)
+      expect(invoice.last['merchant_id']).to eq(merchant.id)
+    end
+    it 'displays all json objects by status' do
+      inv_1 = create(:invoice, status: 'shipped')
+      inv_2 = create(:invoice, status: 'shipped')
+      inv_3 = create(:invoice, status: 'shipped')
+      inv_4 = create(:invoice, status: 'shipped')
+
+      get "/api/v1/invoices/find_all?status=shipped"
+
+      expect(response).to be_successful
+
+      invoice = JSON.parse(response.body)
+
+      expect(invoice.length).to eq(4)
+      expect(invoice.first['status']).to eq(inv_1.status)
+      expect(invoice.last['status']).to eq(inv_4.status)
+    end
+    it 'displays all json objects by created_at' do
+      inv_1 = create(:invoice, created_at: "2012-03-09 08:57:21 UTC")
+      inv_2 = create(:invoice, created_at: "2012-03-09 08:57:21 UTC")
+
+      get "/api/v1/invoices/find_all?created_at=#{inv_1.created_at}"
+
+      expect(response).to be_successful
+
+      invoice = JSON.parse(response.body)
+
+      expect(invoice.length).to eq(2)
+      expect(invoice.first['created_at']).to eq("2012-03-09T08:57:21.000Z")
+      expect(invoice.last['created_at']).to eq("2012-03-09T08:57:21.000Z")
+    end
+    it 'displays all json objects by updated_at' do
+      inv_1 = create(:invoice, updated_at: "2012-03-09 08:57:21 UTC")
+      inv_2 = create(:invoice, updated_at: "2012-03-09 08:57:21 UTC")
+
+      get "/api/v1/invoices/find_all?updated_at=#{inv_1.updated_at}"
+
+      expect(response).to be_successful
+
+      invoice = JSON.parse(response.body)
+
+      expect(invoice.length).to eq(2)
+      expect(invoice.first['updated_at']).to eq("2012-03-09T08:57:21.000Z")
+      expect(invoice.last['updated_at']).to eq("2012-03-09T08:57:21.000Z")
+    end
+  end
 end
