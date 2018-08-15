e2bda2821293ff9085497093e4db1f919c25b724
Eliot Swank <eliotswank@gmail.com>
2018-08-15 08:43:18 -0600

    adds show functionality

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -7,12 +7,25 @@ describe 'Customers API' do
 
       get "/api/v1/customers"
 
+      expect(response).to be_successful
+
       customers = JSON.parse(response.body, symbolize_names: true)
       customer = customers.first
 
       expect(customers.count).to eq(3)
       expect(customer).to have_key(:first_name)
       expect(customer).to have_key(:last_name)
+    end
+
+    it 'returns a single customer' do
+      id = create(:customer).id
+
+      get "/api/v1/customers/#{id}"
+
+      customer = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(customer["id"]).to eq(id)
     end
   end
 end
