1d36b9a6290ef1fa5920e5adf1fcd9955d49de9a
Eliot Swank <eliotswank@gmail.com>
2018-08-15 11:57:26 -0600

    implements find_by_created_at

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -4,5 +4,7 @@ class Api::V1::Customers::SearchController < ApplicationController
     render json: Customer.find(params[:id]) if params[:id]
     render json: Customer.find_by_first_name(params[:first_name]) if params[:first_name]
     render json: Customer.find_by_last_name(params[:last_name]) if params[:last_name]
+    render json: Customer.find_by_created_at(params[:created_at]) if params[:created_at]
+
   end
 end
