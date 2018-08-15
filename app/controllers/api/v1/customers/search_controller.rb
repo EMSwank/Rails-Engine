73e0fd5ba759ee596938acae3024542fe780d72e
Eliot Swank <eliotswank@gmail.com>
2018-08-15 11:49:56 -0600

    implements customer find_by_last_name

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -3,5 +3,6 @@ class Api::V1::Customers::SearchController < ApplicationController
   def show
     render json: Customer.find(params[:id]) if params[:id]
     render json: Customer.find_by_first_name(params[:first_name]) if params[:first_name]
+    render json: Customer.find_by_last_name(params[:last_name]) if params[:last_name]
   end
 end
