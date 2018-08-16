684543cbd4f12c30ed34c5699c567c75bf49ec5b
Eliot Swank <eliotswank@gmail.com>
2018-08-15 22:35:54 -0600

    adds find_all functionailty

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/merchants/find', to: 'merchants/search#show'
+      get '/merchants/find_all', to: 'merchants/search#index'
       resources :merchants, only: [:index, :show]
+
       get '/api/v1/transactions/find?', to: 'transactions#show'
       resources :transactions, only: [:index, :show]
+
       get '/invoices/find', to: 'invoices/search#show'
       resources :invoices, only: [:index, :show]
+      
       get '/customers/find', to: 'customers/search#show'
       get '/customers/find_all', to: 'customers/search#index'
       resources :customers, only: [:index, :show]
--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -70,4 +70,61 @@ describe 'Merchants API' do
       expect(merchant["updated_at"]).to eq("2012-03-27T14:54:05.000Z")
     end
   end
+
+  context "GET /api/v1/merchants/find_all?" do
+    it 'finds all merchants by ID through query params' do
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant)
+      merchant3 = create(:merchant)
+
+      get "/api/v1/merchants/find_all?id=#{merchant1.id}"
+      
+      expect(response).to be_successful
+      
+      merchant = JSON.parse(response.body)
+
+      expect(merchant.length).to eq(1)
+    end
+
+    it 'finds all merchants by name through query params' do
+      name = create(:merchant).name
+
+      get "/api/v1/merchants/find_all?name=#{name}"
+
+      expect(response).to be_successful
+
+      merchant = JSON.parse(response.body)
+
+      expect(merchant[0]["name"]).to eq(name)
+    end
+
+    it 'finds all merchants by created_at through query params' do
+      merchant1 = create(:merchant, created_at: "2012-03-27 14:56:35 UTC")
+      merchant2 = create(:merchant)
+      merchant3 = create(:merchant)
+
+      get "/api/v1/merchants/find_all?created_at=#{merchant1.created_at}"
+
+      expect(response).to be_successful
+
+      merchant = JSON.parse(response.body)
+
+      expect(merchant[0]["id"]).to eq(merchant1.id)
+    end
+
+    it 'finds all merchants by updated_at through query params' do
+      merchant1 = create(:merchant, updated_at: "2012-03-27 14:56:35 UTC")
+      merchant2 = create(:merchant)
+      merchant3 = create(:merchant)
+
+      get "/api/v1/merchants/find_all?updated_at=#{merchant1.updated_at}"
+
+      expect(response).to be_successful
+
+      merchant = JSON.parse(response.body)
+
+      expect(merchant[0]["id"]).to eq(merchant1.id)
+    end
+  end
+
 end
