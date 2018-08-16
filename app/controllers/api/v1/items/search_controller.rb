8bd5cd23c54dd6fbd09e5a9402ca981fe2b4f9e8
Chris Powell <chris.ian.powell@gmail.com>
2018-08-15 18:21:57 -0600

    adds fixes to items endpoints to pass spec harness

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -3,10 +3,10 @@ class Api::V1::Items::SearchController < ApplicationController
     render json: Item.find(params[:id]) if params[:id]
     render json: Item.find_by_name(params[:name]) if params[:name]
     render json: Item.find_by_description(params[:description]) if params[:description]
-    render json: Item.find_by_unit_price(params[:unit_price]) if params[:unit_price]
+    render json: Item.find_by_unit_price((params[:unit_price].to_d * 100).to_i) if params[:unit_price]
     render json: Item.find_by_merchant_id(params[:merchant_id]) if params[:merchant_id]
-    render json: Item.find_by_created_at(params[:created_at]) if params[:created_at]
-    render json: Item.find_by_updated_at(params[:updated_at]) if params[:updated_at]
+    render json: Item.order(:id).find_by_created_at(params[:created_at]) if params[:created_at]
+    render json: Item.order(:id).find_by_updated_at(params[:updated_at]) if params[:updated_at]
   end
 
   def index
@@ -14,7 +14,7 @@ class Api::V1::Items::SearchController < ApplicationController
     render json: Item.where('name=?', params[:name]) if params[:name]
     render json: Item.where('description=?', params[:description]) if params[:description]
     render json: Item.where('merchant_id=?', params[:merchant_id]) if params[:merchant_id]
-    render json: Item.where('unit_price=?', params[:unit_price]) if params[:unit_price]
+    render json: Item.where('unit_price=?', (params[:unit_price].to_d * 100).to_i) if params[:unit_price]
     render json: Item.where('created_at=?', params[:created_at]) if params[:created_at]
     render json: Item.where('updated_at=?', params[:updated_at]) if params[:updated_at]
   end
--- app/serializers/invoice_serializer.rb
+++ app/serializers/invoice_serializer.rb
@@ -1,5 +1,4 @@
 class InvoiceSerializer < ActiveModel::Serializer
   attributes :id, :customer_id, :merchant_id, :status
 
-  has_many :items
 end
