6e5dded54ae3480ad913cbf47031fe27b98e81bd
Eliot Swank <eliotswank@gmail.com>
2018-08-15 21:52:25 -0600

    search controller refactor

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
-    render json: Merchant.find(params[:id]) if params[:id]
-    render json: Merchant.find_by_name(params[:name]) if params[:name]
-    render json: Merchant.find_by_created_at(params[:created_at]) if params[:created_at]
-    render json: Merchant.find_by_updated_at(params[:updated_at]) if params[:updated_at]
+    render json: Merchant.find(search_params)
   end
+
+  def index
+    render json: Merchant.where(search_params)
+  end
+  private
+
+  def search_params
+    params.permit(:id, :name, :created_at, :updated_at)
+  end
+  
 end
