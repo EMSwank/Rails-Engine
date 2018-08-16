0045a6c0743f0c4c82475da4f1a1420f60ee4d54
Eliot Swank <eliotswank@gmail.com>
2018-08-15 22:55:34 -0600

    adds route for find_params

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
       get '/merchants/find_all', to: 'merchants/search#index'
       resources :merchants, only: [:index, :show]
 
-      get '/api/v1/transactions/find?', to: 'transactions#show'
+      get '/transactions/find', to: 'transactions/search#show'
       resources :transactions, only: [:index, :show]
 
       get '/invoices/find', to: 'invoices/search#show'
       resources :invoices, only: [:index, :show]
-      
+
       get '/customers/find', to: 'customers/search#show'
       get '/customers/find_all', to: 'customers/search#index'
       resources :customers, only: [:index, :show]
